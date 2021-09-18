a3e7459556044e39a8ba70656f32bc2b0f4b8b65
shaun <shaunforfun20@gmail.com>
2021-09-18 18:53:37 -0400

    adding boostrap to project

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
+environment.plugins.append('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        Poppper: ['popper.js', 'default']
+    })
+)
 
 module.exports = environment
