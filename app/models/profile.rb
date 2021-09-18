52cbde957eb943c430b0520d528e393663f5b3f5
shaun <shaunforfun20@gmail.com>
2021-09-18 18:54:47 -0400

    profile updates

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -22,4 +22,8 @@
 #
 class Profile<ApplicationRecord
     belongs_to :user
+
+    def name 
+        self.first_name + " " + self.last_name
+    end 
 end
--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -1,14 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "Profiles", type: :request do
-  describe "GET profiles/new" do
-    it"creates profile and redirect to show " do
-      user = User.create(email:"abc@gmail.com",password:"password") 
-      post "/users/#{user.id}/profiles", params:{user_id: user.id,profile: {first_name:  "Boppy"}}
-      expect(response).to redirect_to(user_profile_url(user.id,assigns(:profile)))
-      follow_redirect!
-      expect(response).to render_template(:show)
-      expect(response.body).to include("This is profile show page")
-    end
-  end
 end
