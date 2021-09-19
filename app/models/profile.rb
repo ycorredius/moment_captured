23357ad11bfd843589979eaa161f23e64a16dcd2
shaun <shaunforfun20@gmail.com>
2021-09-18 21:44:04 -0400

    adding photo capability to model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -23,6 +23,9 @@
 class Profile<ApplicationRecord
     belongs_to :user
 
+    has_one_attached :avatar
+    has_many_attached :photographs
+
     def name 
         self.first_name + " " + self.last_name
     end 
