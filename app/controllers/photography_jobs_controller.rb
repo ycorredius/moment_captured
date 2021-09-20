c67c3e30a23da41eaa656629c41062fe3beefcb6
shaun <shaunforfun20@gmail.com>
2021-09-20 18:01:12 -0400

    updating photography jobs

--- app/controllers/photography_jobs_controller.rb
+++ app/controllers/photography_jobs_controller.rb
@@ -1,2 +1,10 @@
 class PhotographyJobsController < ApplicationController
+    # before_action :set_user, only: %i[ new create ]
+    def index 
+        @jobs = PhotographyJob.all
+    end
+
+    def new
+       @job = PhotographyJob.new
+    end
 end
